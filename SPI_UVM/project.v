ac92582a4ee147bc7a702255a48c838c387f3c9b
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-07-23 14:12:41 +0300

    Delete SPI_UVM directory

--- SPI_UVM/project.v
+++
@@ -1,9 +0,0 @@
-module project (MOSI,MISO,SS_n,clk,rst_n);
-input MOSI,clk,rst_n,SS_n;
-output MISO;
-wire rx_valid,tx_valid;
-wire[7:0]tx_data;
-wire [9:0]rx_data;
-SPI dut(MOSI,MISO,SS_n,clk,rst_n,rx_data,rx_valid,tx_data,tx_valid);
-ram dut1(rx_data,clk,rst_n,rx_valid,tx_data,tx_valid);
-endmodule
--- SPI_UVM/ram_if.sv
+++
@@ -1,8 +0,0 @@
-import pkg::*;
-interface ram_if (clk);
-	input clk;
-	logic [9:0]din;
-logic rst_n,rx_valid;
-logic  [ADDER_SIZE-1:0]dout;
-logic  tx_valid;
-endinterface 
