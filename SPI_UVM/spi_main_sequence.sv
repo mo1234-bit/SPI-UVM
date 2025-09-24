9307963860c280306d8c1e6217b783521a70f63c
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-09-24 23:56:03 +0300

    Update spi_main_sequence.sv

--- SPI_UVM/spi_main_sequence.sv
+++ SPI_UVM/spi_main_sequence.sv
@@ -5,6 +5,7 @@ package spi_main_sequence;
 class spi_main_sequence extends uvm_sequence #(spi_seq_item);
 	`uvm_object_utils(spi_main_sequence)
 	spi_seq_item seq_item;
+	logic [1:0] prev_opcode=2'bxx;
 	function  new(string name="spi_main_sequence");
 		super.new(name);
 	endfunction 
@@ -13,7 +14,20 @@ class spi_main_sequence extends uvm_sequence #(spi_seq_item);
 		repeat(10000)begin
 			seq_item=spi_seq_item::type_id::create("seq_item");
 			start_item(seq_item);
-			assert(seq_item.randomize());
+			 if (prev_opcode == 2'b00) begin
+          assert(seq_item.randomize() with { container[9:8] == 2'b01; })
+          prev_opcode = 2'b01;
+        end
+        else if (prev_opcode == 2'b10) begin
+          assert(seq_item.randomize() with { container[9:8] == 2'b11; })
+          prev_opcode = 2'b11;
+
+        end
+        else begin
+          assert(seq_item.randomize())
+          prev_opcode = seq_item.container[9:8];
+        end
+
 			finish_item(seq_item);
 		end
 	endtask : body
