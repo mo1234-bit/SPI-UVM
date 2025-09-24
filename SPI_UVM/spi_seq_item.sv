34632ef1add30e13e104703756dd215f7cfdabaa
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-09-24 23:56:50 +0300

    Update spi_seq_item.sv

--- SPI_UVM/spi_seq_item.sv
+++ SPI_UVM/spi_seq_item.sv
@@ -3,33 +3,17 @@ package spi_seq_item;
 `include "uvm_macros.svh";
 class spi_seq_item extends  uvm_sequence_item;
 	`uvm_object_utils(spi_seq_item)
-	rand logic[10:0]container;
+	rand logic[9:0]container;
 	function  new(string name ="spi_seq_item");
 		super.new(name);
-container=0;
+
 	endfunction 
-rand logic MOSI,SS_n,rst_n;
+rand logic SS_n,rst_n;
 logic MISO;
 logic MISO_ref;
-logic add_sent;
-function void pre_randomize();
-	if(!rst_n)
-		add_sent=0;
-	else begin
-		if(container[10:8]==3'b110)
-			add_sent=1;
-		else
-			add_sent=0;
-	end
-     
-endfunction 
-constraint data {
-	if(container[10]&&add_sent){
-		container[9:8]==2'b11;
-	}else{
-	container[10:8] inside {3'b110,3'b001,3'b000};
-	}
-}
+logic add_sent,add_sent_read;
+logic MOSI;
+
 
 constraint a {
 	SS_n dist {1:=20,0:=80};
